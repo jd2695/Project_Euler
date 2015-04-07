e59bc12e760d6938e73ef7377decfec250b1a27b
Jim <jimdong@nyu.edu>
2015-04-07 15:12:07 -0400

    Initial

---
+++ Euler_Sol_1.py
@@ -0,0 +1,12 @@
+﻿# By: Jim Dong
+# Solution for Project Euler problem 1
+# Naive algorithm, runs on O(n) where n is the input number
+
+start = 0
+sum = 0
+while ( start != 1000):
+    if (start % 3 == 0 or start % 5 == 0) :
+        sum +=start
+    start += 1
+print sum
+
